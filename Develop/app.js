b91be45757e427fbdf0ac3e98dcd7187671d2794
Michael Evans <Jawsbreaker@Michaels-MacBook-Pro.local>
2021-02-26 16:06:27 -0700

    added local storagesupport

--- Develop/app.js
+++ Develop/app.js
@@ -19,17 +19,29 @@ let inFuture = $('.future')
 //my moment object is my missing piece
 //comparing two moment objects
 //selecting text area with ta-block
+//element = <textarea>
 $(".ta-block").each((index, element) => {
+
+    const id = element.id
+
     //make object
     const hour =  moment({
-        hour: element.id
+        hour: id
     })
     //actual hour
-    console.log(moment().get('hour'))
+    //console.log(moment().get('hour'))
     //object hour 
-    console.log(hour.get('hour'))
+    //console.log(hour.get('hour'))
     //compare these two so I can use 'present' class
     //console.log(element)
+
+    const value = localStorage.getItem(id)
+    //value in dom element 
+    element.value = value
+    
+
+
+
     if (moment().get('hour') === hour.get('hour')){
         $(element).addClass('present')
     }
@@ -41,12 +53,24 @@ $(".ta-block").each((index, element) => {
 })
 
 //TODO: this 2/25
+$('.sections').on('click', 'button', function(e){
+    //closest parent that text area shares
+    const ta = $(e.target).closest('section')
+        .find('textarea')
+    console.log(ta)
+    const id = ta.attr('id')
+    const value = ta.val()
+    console.log(id, value)
+    //use id as key so i can use id of each textarea
+    //then use value to grab actual text
+    localStorage.setItem(id, value)
+})
 //the things we need - input
-console.log($('.saveBtn'))
-console.log($('.ta-block'))
+//console.log($('.saveBtn'))
+//console.log($('.ta-block'))
 //console log every button in array - pass function into each
 //setting parameter through another this,
-$('.saveBtn').each(index, saveBtnEl) => console.log(saveBtnEl)
+//$('.saveBtn').each(index, saveBtnEl) => console.log(saveBtnEl)
 // console.log($(saveBtn))
 
 //print clock
